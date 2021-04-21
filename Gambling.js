6fdaca0230b466c3d6ab53b38ee07e91d2fa6994
“siddiq76” <siddiq.khan0786@gmail.com>
2021-04-21 13:10:42 +0530

    UC2 Win or loss

--- Gambling.js
+++ Gambling.js
@@ -2,3 +2,16 @@ console.log("welcome to the game");
 
 const stake = 100;
 const bet = 1;
+
+const win = 1;
+const loss = 0;
+
+function winnerDecider() {
+    let decider = Math.floor(Math.random() * 2);
+    if (decider == 0){
+        console.log("Lost");
+    }else {
+        console.log("won")
+    }
+}
+ winnerDecider();
